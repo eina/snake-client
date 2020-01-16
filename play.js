0131467e27d29a9e4fa5485761f951f6a5efe10e
Eina Onting <einers@outlook.com>
2020-01-15 16:51:29 -0800

    setup and handle user input

--- play.js
+++ play.js
@@ -2,3 +2,22 @@ const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+/**
+ * Set Up User Interface
+ */
+const handleUserInput = () => {
+  if(key === '\u0003') {
+    process.stdout.write(`Thanks for using me, ciao!\n`);
+    process.exit();
+  }
+}
+
+const setupInput = () => {
+  const stdin = process.stdin;    
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();  
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
