50fbc9d094f71d3330a3292bce593b7110599018
Eina Onting <einers@outlook.com>
2020-01-15 17:17:56 -0800

    implement user input controls

--- input.js
+++ input.js
@@ -1,13 +1,29 @@
 /**
  * Set Up User Interface
  */
+// Stores the active TCP connection object.
+let connection;
+
 const handleUserInput = (key) => {
   if(key === '\u0003') {    
     process.exit();
+  } else if (key === '\u0077') {
+    // w
+    connection.write('Move: up')
+  } else if (key === '\u0061') {
+    // a
+    connection.write('Move: left')
+  } else if (key === '\u0073') {
+    // s
+    connection.write('Move: down')
+  } else if (key === '\u0064') {
+    // d
+    connection.write('Move: right')
   }
 }
 
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;    
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
--- play.js
+++ play.js
@@ -2,6 +2,5 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-connect();
 
-setupInput();
+setupInput(connect());
