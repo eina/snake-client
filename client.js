5d716b9599d76fa235a40d8c1208b8128d53acd4
Eina Onting <einers@outlook.com>
2020-01-15 16:00:26 -0800

    add name for snake

--- client.js
+++ client.js
@@ -12,6 +12,11 @@ const connect = function(){
   //interpret incoming data as text
   conn.setEncoding('utf8');
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server!');
+    conn.write('Name: WoL');
+  })
+
   conn.on('data', (data) => {
     console.log(data)
   })
