f933ed735e19415724737f8774d93065b01bcc7d
Eina Onting <einers@outlook.com>
2020-01-15 17:44:02 -0800

    show special messages when pressing keys

--- input.js
+++ input.js
@@ -5,6 +5,8 @@
 let connection;
 
 const handleUserInput = (key) => {
+  console.log('okay but what are you', key)
+  // \u0047 => g
   if(key === '\u0003') {    
     process.exit();
   } else if (key === '\u0077') {
@@ -19,6 +21,14 @@ const handleUserInput = (key) => {
   } else if (key === '\u0064') {
     // d
     connection.write('Move: right')
+  }
+
+  if(key === 'f') {
+    connection.write('Say: f in chat');
+  } else if (key === 'g') {
+    connection.write('Say: gg');
+  } else if (key === 'l') {
+    connection.write('Say: glhf!!!!!')
   }
 }
 
